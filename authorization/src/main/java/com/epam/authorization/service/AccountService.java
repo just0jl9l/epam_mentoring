bd40c3c80946c35a8ecfe7a5982933f97f95328a
Volha Charnykh <Volha_Charnykh@epam.com>
2017-03-28 09:58:09 +0300

    service interface created

--- authorization/src/main/java/com/epam/authorization/service/AccountService.java
+++ authorization/src/main/java/com/epam/authorization/service/AccountService.java
@@ -1,38 +1,12 @@
 package com.epam.authorization.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.epam.authorization.dao.AccountDao;
-import com.epam.authorization.dao.exception.DaoException;
 import com.epam.authorization.domain.Account;
 import com.epam.authorization.service.exception.DataNotFoundException;
 import com.epam.authorization.service.exception.GettingDataException;
 import com.epam.authorization.service.exception.WrongDataException;
 
-@Service("accountService")
-public class AccountService {
-	
-	@Autowired
-	private AccountDao accountDao;
+public interface AccountService {
 
-	@Transactional
-	public Account authorization(String login, String password)
-			throws WrongDataException, GettingDataException, DataNotFoundException {
-		if (login != null && password != null && !"".equals(login) && !"".equals(password)) {
-			try {
-				Account account = accountDao.get(login, password);
-				if (account != null) {
-					return account;
-				} else {
-					throw new DataNotFoundException("Account wasn't found");
-				}
-			} catch (DaoException e) {
-				throw new GettingDataException("DaoException occurred during getting account", e);
-			}
-		} else {
-			throw new WrongDataException("Empty parameters");
-		}
-	}
+	Account authorization(String login, String password)
+			throws WrongDataException, GettingDataException, DataNotFoundException;
 }
---
+++ authorization/src/main/java/com/epam/authorization/service/impl/AccountServiceImpl.java
@@ -0,0 +1,40 @@
+package com.epam.authorization.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.epam.authorization.dao.AccountDao;
+import com.epam.authorization.dao.exception.DaoException;
+import com.epam.authorization.domain.Account;
+import com.epam.authorization.service.AccountService;
+import com.epam.authorization.service.exception.DataNotFoundException;
+import com.epam.authorization.service.exception.GettingDataException;
+import com.epam.authorization.service.exception.WrongDataException;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+	
+	@Autowired
+	private AccountDao accountDao;
+
+	@Override
+	@Transactional
+	public Account authorization(String login, String password)
+			throws WrongDataException, GettingDataException, DataNotFoundException {
+		if (login != null && password != null && !"".equals(login) && !"".equals(password)) {
+			try {
+				Account account = accountDao.get(login, password);
+				if (account != null) {
+					return account;
+				} else {
+					throw new DataNotFoundException("Account wasn't found");
+				}
+			} catch (DaoException e) {
+				throw new GettingDataException("DaoException occurred during getting account", e);
+			}
+		} else {
+			throw new WrongDataException("Empty parameters");
+		}
+	}
+}
