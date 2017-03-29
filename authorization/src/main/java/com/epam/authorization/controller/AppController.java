f852fb9d9b40b9054effeb45c13924b8437b4e81
Volha Charnykh <Volha_Charnykh@epam.com>
2017-03-29 09:52:50 +0300

    controller

--- authorization/src/main/java/com/epam/authorization/controller/AppController.java
+++ authorization/src/main/java/com/epam/authorization/controller/AppController.java
@@ -1,69 +1,78 @@
-//package com.epam.authorization.controller;
-//
-//import java.util.List;
-//import java.util.Locale;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.MessageSource;
-//import org.springframework.security.authentication.AuthenticationTrustResolver;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.FieldError;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.SessionAttributes;
-//
-//import com.epam.authorization.service.AccountService;
-//
-//@Controller
-//@RequestMapping("/")
-//@SessionAttributes("roles")
-//public class Controller {
-//	@Autowired
-//	AccountService accountService;
-//
-//	@Autowired	
-//	AccountRoleService accountRoleService;
-//
-//	
-//	@RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
-//	public String accessDeniedPage(ModelMap model) {
-//		model.addAttribute("loggedinuser", getPrincipal());
-//		return "accessDenied";
-//	}
-//
-//	/**
-//	 * This method handles login GET requests. If users is already logged-in and
-//	 * tries to goto login page again, will be redirected to list page.
-//	 */
-//	@RequestMapping(value = "/login", method = RequestMethod.GET)
-//	public String loginPage() {
-//		if (isCurrentAuthenticationAnonymous()) {
-//			return "login";
-//		} else {
-//			return "redirect:/list";
-//		}
-//	}
-//
-//
-//	private String getPrincipal() {
-//		String userName = null;
-//		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//		if (principal instanceof UserDetails) {
-//			userName = ((UserDetails) principal).getUsername();
-//		} else {
-//			userName = principal.toString();
-//		}
-//		return userName;
-//	}
-//}
+package com.epam.authorization.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView defaultPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Login Form - Database Authentication");
+		model.addObject("message", "This is default page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Login Form - Database Authentication");
+		model.addObject("message", "This page is for ROLE_ADMIN only!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
+	}
+	
+	//for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
+		ModelAndView model = new ModelAndView();
+		
+		//check if user is login
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			UserDetails userDetail = (UserDetails) auth.getPrincipal();
+			System.out.println(userDetail);
+		
+			model.addObject("username", userDetail.getUsername());
+			
+		}
+		
+		model.setViewName("403");
+		return model;
+
+	}
+}
